3d3845f52f5ad6febd3ddfac3eea734ae8b47be9
Davi <davirodrigues8@gmail.com>
2019-01-28 23:43:29 -0300

    Removendo rota de salvar dados no bd

--- portal/core/urls.py
+++ portal/core/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     # URL para buscar as noticias do banco de dados
-    url(r'^noticias$', 'portal.core.views.index', name='index'),
-    # URL para salvar as noticias no banco de dados
-    url(r'^$', 'portal.core.views.store', name='store')
+    url(r'^$', 'portal.core.views.index', name='index'),
 ]
--- portal/core/views.py
+++ portal/core/views.py
@@ -4,9 +4,6 @@ from .forms import SearchNotice
 # Imports redirect
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
-# Imports webscraping
-from bs4 import BeautifulSoup
-import requests
 
 def index(request):
     notices = Notice.objects.all()
@@ -32,25 +29,3 @@ def index(request):
         'msg': msg
     }
     return render(request, template_name, context)
-
-# Buscar as noticias do tecmundo e salvar no banco de dados
-def store(request):
-    # Fazendo requisição ao site do tecmundo
-    html =  requests.get('https://www.tecmundo.com.br/').text
-    # Criando objeto usando biblioteca html5
-    soup = BeautifulSoup(html, 'html5lib')
-    # Buscando as divs do carousel que possui as principais noticias
-    data = soup.find_all('div',attrs={'class':'nzn-main-text'})
-    # Criando array de dados extraidos
-    extracted_data = []
-
-    # Percorrendo os elementos filhos
-    for child in data:
-        # Extraindo primeira parte
-        p1 = child.find('strong').text
-        # Extraindo segunda parte
-        p2 = child.find('h2').text
-        # Criando um objeto noticia com titulo concatenado
-        Notice.objects.get_or_create(title=p1+' '+p2)
-    # Redirecionando para a pagina index
-    return redirect(reverse('core:index'))
